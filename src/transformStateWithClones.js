51b912ce9219c5f14703c368cee4a848e640f05f
Boris Mund <boris17527684@gmail.com>
2025-08-07 22:19:26 +0300

    stateful-clones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,25 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+  let curentStay = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      curentStay = { ...curentStay, ...action.extraData };
+      result.push({ ...curentStay });
+    } else if (action.type === 'clear') {
+      curentStay = {};
+      result.push({ ...curentStay });
+    } else if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete curentStay[key];
+      }
+      result.push({ ...curentStay });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
